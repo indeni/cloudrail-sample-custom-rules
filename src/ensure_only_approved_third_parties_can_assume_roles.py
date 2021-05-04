9152d87f06b9cf925f8f81d777762dd1dd06d5e8
Yoni Leitersdorf <y@indeni.com>
2021-05-04 08:44:16 -0700

    First rule, initial commit

---
+++ src/ensure_only_approved_third_parties_can_assume_roles.py
@@ -0,0 +1,36 @@
+from typing import List, Dict
+from cloudrail.knowledge.context.aws.account.account import Account
+from cloudrail.knowledge.context.aws.iam.policy_statement import StatementEffect
+from cloudrail.knowledge.context.environment_context import EnvironmentContext
+from cloudrail.knowledge.rules.base_rule import BaseRule, Issue
+from cloudrail.knowledge.rules.rule_parameters.base_paramerter import ParameterType
+from cloudrail.knowledge.utils import arn_utils
+
+
+class EnsureOnlyAssumesThirdPartiesCanAssumeRoles(BaseRule):
+    def __init__(self):
+        self.approved_list_of_third_parties: List[str] = [
+            "645376637575",  # Indeni Cloudrail
+            "464622532012",  # DataDog
+        ]
+
+    def get_id(self) -> str:
+        return 'ensure_only_approved_third_parties_can_assume_roles'
+
+    def get_needed_parameters(self) -> List[ParameterType]:
+        return []
+
+    def execute(self, env_context: EnvironmentContext, parameters: Dict[ParameterType, any]) -> List[Issue]:
+        issues: List[Issue] = []
+
+        for role in env_context.roles:
+            for statement in role.assume_role_policy.get_all_statements():
+                if statement.effect == StatementEffect.ALLOW:
+                    for principal in statement.principal.principal_values:
+                        if arn_utils.is_valid_arn(principal) and arn_utils.get_arn_account_id(principal) not in self.approved_list_of_third_parties:
+                            issues.append(Issue(f'The IAM role `{role.get_friendly_name()}` has a trust policy that allows account `{ arn_utils.get_arn_account_id(principal)}` '
+                                        f'to assume it but that is not in the list of pre-approved third-party accounts', role, role))
+        return issues
+
+    def should_run_rule(self, environment_context: EnvironmentContext) -> bool:
+        return bool(environment_context.roles)
---
+++ test/test_ensure_only_approved_third_parties_can_assume_roles.py
@@ -0,0 +1,72 @@
+import unittest
+
+from cloudrail.knowledge.context.aws.account.account import Account
+from cloudrail.knowledge.context.aws.iam.policy import AssumeRolePolicy
+from cloudrail.knowledge.context.aws.iam.policy_statement import PolicyStatement, StatementEffect
+from cloudrail.knowledge.context.aws.iam.principal import Principal, PrincipalType
+from cloudrail.knowledge.context.aws.iam.role import Role
+from cloudrail.knowledge.context.environment_context import EnvironmentContext
+from cloudrail.knowledge.rules.base_rule import RuleResultType
+from cloudrail.dev_tools.rule_test_utils import create_empty_entity
+from src.ensure_only_approved_third_parties_can_assume_roles import EnsureOnlyAssumesThirdPartiesCanAssumeRoles
+
+
+class TestEnsureOnlyAssumesThirdPartiesCanAssumeRoles(unittest.TestCase):
+    def setUp(self):
+        self.rule = EnsureOnlyAssumesThirdPartiesCanAssumeRoles()
+
+    def test_ensure_only_approved_third_parties_can_assume_roles_fail(self):
+        # Arrange
+        context = EnvironmentContext()
+
+        account = Account("a", "b", False)
+        context.accounts.append(account)
+
+        role = Role("a", "don't know", "not_approved_role", [], "not_approved_role", None, None)
+        context.roles.append(role)
+
+        principal = Principal(principal_type = PrincipalType.AWS, principal_values = ["arn:aws:iam::123456789012:root"])
+
+        role_assume_policy = AssumeRolePolicy(account.account_name, role.role_name,
+                 role.arn, [PolicyStatement(
+                 StatementEffect.ALLOW,
+                 ["assume role etc"],
+                 ["*"],
+                 principal,
+                 'test123',)], "")
+        role.assume_role_policy = role_assume_policy
+
+        # Act
+        result = self.rule.run(context, {})
+
+        # Assert
+        self.assertEqual(RuleResultType.FAILED, result.status)
+        self.assertEqual(1, len(result.issues))
+
+    def test_ensure_only_approved_third_parties_can_assume_roles_pass(self):
+        # Arrange
+        context = EnvironmentContext()
+
+        account = Account("a", "b", False)
+        context.accounts.append(account)
+
+        role = Role("a", "don't know", "not_approved_role", [], "not_approved_role", None, None)
+        context.roles.append(role)
+
+        principal = Principal(principal_type=PrincipalType.AWS, principal_values=["arn:aws:iam::645376637575:root"])
+
+        role_assume_policy = AssumeRolePolicy(account.account_name, role.role_name,
+                                              role.arn, [PolicyStatement(
+                StatementEffect.ALLOW,
+                ["assume role etc"],
+                ["*"],
+                principal,
+                'test123', )], "")
+        role.assume_role_policy = role_assume_policy
+
+        # Act
+        result = self.rule.run(context, {})
+
+        # Assert
+        self.assertEqual(RuleResultType.SUCCESS, result.status)
+        self.assertEqual(0, len(result.issues))
