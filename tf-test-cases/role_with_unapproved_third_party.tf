684bd53f934a094cc301ef5b80add10a40b0d2d5
Yoni Leitersdorf <y@indeni.com>
2021-05-13 14:49:27 -0700

    Add first working rule (#1)

---
+++ tf-test-cases/role_with_unapproved_third_party.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_role" "unapproved_third_party" {
+  name = "unapproved_third_party"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    },
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::111122223333:user/LiJuan"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
